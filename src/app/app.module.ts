8e8bbec5524d54bf10eb7a4d0ada13cae781ed9d
bersling <bersling@gmail.com>
2017-07-03 11:19:21 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {HeroModule} from './hero/index';
 
-import { createResourceModule } from '@tsmean/resource';
+import { ResourceModule } from '@tsmean/resource';
 
 import {NotifyModule} from 'notify-angular';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MdButtonModule} from '@angular/material';
-const resourceModule = createResourceModule('http://demo.tsmean.com:4242/api/v1');
+
 
 @NgModule({
   declarations: [
@@ -18,7 +18,7 @@ const resourceModule = createResourceModule('http://demo.tsmean.com:4242/api/v1'
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    resourceModule,
+    ResourceModule.forRoot('http://demo.tsmean.com:4242/api/v1'),
     NotifyModule.forRoot(),
     HeroModule.forRoot(),
     MdButtonModule
